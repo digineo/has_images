8e4d7c6221e5b95a485dc29cd38f852ee7c93f85
Dennis Meise <dm@digineo.de>
2010-03-17 12:11:52 +0100

    Interpolations korrigiert

--- init.rb
+++ init.rb
@@ -2,14 +2,14 @@
 require 'has_images'
 ActiveRecord::Base.send(:include, HasImages)
 
-Paperclip.interpolates :appname do |attachment, style| 
+Paperclip.interpolates :short_id_partition do |attachment, style| 
   ("%08d" % attachment.instance.id).scan(/\d{4}/).join("/")
 end
 
-Paperclip.interpolates :appname do |attachment, style| 
+Paperclip.interpolates :parent do |attachment, style| 
   attachment.instance.parentmodel.image_folder rescue attachment.instance.parentmodel.class.to_s.underscore.pluralize
 end
 
-Paperclip.interpolates :appname do |attachment, style| 
+Paperclip.interpolates :parent_name do |attachment, style| 
   attachment.instance.parentmodel.seo_name
 end
