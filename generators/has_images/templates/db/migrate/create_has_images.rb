42e8b131f3f006c061421ef14f214cf9d7f598f5
Dennis Meise <dm@digineo.de>
2010-03-17 10:23:51 +0100

    Fehlendes Feld in der Image-Migration hinzugefügt

--- generators/has_images/templates/db/migrate/create_has_images.rb
+++ generators/has_images/templates/db/migrate/create_has_images.rb
@@ -22,6 +22,7 @@ class CreateHasImages < ActiveRecord::Migration
       t.string :friendly_name, :null => false
       t.string :file_file_name
       t.string :file_content_type
+      t.string :file_remote_url
       t.integer :file_file_size
       t.datetime :file_updated_at
       t.string :type
